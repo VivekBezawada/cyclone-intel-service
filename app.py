4bb5b74485578d1045960e137f44bde503a63e59
VivekBezawada <bezawadavivek@gmail.com>
2020-05-06 22:20:43 +0530

    Ading docker and docker compose for server and dfb

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 import json
 
+# This config can come up from an external source
+# or via command line suing Consul and Vault to handle different environments
+# for simplicity, Leaving the config with the values
 config = json.load(open('./config.json', 'r'))
 
 app = Flask(__name__)
