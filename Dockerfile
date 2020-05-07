a0e752d6df798e931c2e30690a545384ae0ec23c
VivekBezawada <bezawadavivek@gmail.com>
2020-05-08 02:23:40 +0530

    Docker Compose bug fixes; Inserting data into DB

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 from python:3.7-alpine
 
-RUN apk add --no-cache python3-dev \
-    && pip3 install --upgrade pip
+RUN apk add python3-dev gcc musl-dev libxml2-dev libxslt-dev postgresql-dev\
+    && pip3 install --upgrade pip\
+    && pip3 install psycopg2
---
+++ db_schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE "cyclone-intel";
+\c "cyclone-intel"
+CREATE TABLE cyclone_info(
+    cyclone_id TEXT PRIMARY KEY NOT NULL,
+    cyclone_name TEXT NOT NULL,
+    region TEXT NOT NULL,
+    cyclone_status BOOLEAN DEFAULT TRUE
+);
+\d
+CREATE TABLE track_data(
+    cyclone_id TEXT REFERENCES cyclone_info(cyclone_id),
+    synoptic_time BIGINT NOT NULL,
+    latitude FLOAT,
+    longitude FLOAT,
+    intensity INT,
+    UNIQUE(cyclone_id, synoptic_time)
+);
+\d
+CREATE TABLE forecast_data(
+    cyclone_id TEXT REFERENCES cyclone_info(cyclone_id),
+    forecast_time BIGINT NOT NULL,
+    latitude FLOAT,
+    longitude FLOAT,
+    intensity INT,
+    UNIQUE(cyclone_id, forecast_time)
+);
+\d
